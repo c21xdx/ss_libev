b9b51566026ee1a408dcad973bfd5f792bccb3ff
c21xdx <c21xdx@gmail.com>
2022-03-01 16:54:29 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ sed -i "s/ss_passwd/$SS_PASSWD/" /etc/shadowsocks-libev/v2r.json
 sed -i "s/10086/$PORT/" /etc/shadowsocks-libev/xray.json
 sed -i "s/ss_passwd/$SS_PASSWD/" /etc/shadowsocks-libev/xray.json
 
-ss-server -c /etc/shadowsocks-libev/$CONFIG_JSON
+ss-server -c /etc/shadowsocks-libev/{$CONFIG_JSON}.json
